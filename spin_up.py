3b679fae0d1429f0d6ad51c5d6e2eb3145f17abc
Micah Abbott <miabbott@redhat.com>
2015-02-06 23:20:31 -0500

    Generating cidata ISO, starting VM

--- spin_up.py
+++ spin_up.py
@@ -6,22 +6,58 @@ import re
 import subprocess
 import sys
 
+
+def generate_ci_iso(name=None, user_data=None, meta_data=None, image_dir=None):
+    if name is None or user_data is None:
+        print ("ERROR: need to provide an instance name and the path to" +
+               "user-data file")
+        return 1
+
+    if not os.path.isfile(user_data):
+        print ("ERROR: could not find user-data file at specified " +
+               "location: %s" % user_data)
+
+    iso_name = name + "-cidata.iso"
+    if image_dir is None:
+        fp_to_iso = "/var/lib/libvirt/images/" + iso_name
+    else:
+        fp_to_iso = image_dir + iso_name
+
+    if meta_data is None:
+        md_f = open('/tmp/meta-data', 'w')
+        md_f.write("instance-id: %s\n" % name)
+        md_f.write("local-hostname: %s\n" % name)
+        md_f.close()
+        meta_data = '/tmp/meta-data'
+
+    cmd = ["/usr/bin/genisoimage", "-output", fp_to_iso, "-volid", "cidata",
+           "-joliet", "-rock", user_data, meta_data]
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    if p.returncode != 0:
+        print "ERROR: unable to generate cidata ISO"
+        print err
+        return 1
+
+    return fp_to_iso
+
 IMAGES_DIR = '/var/lib/libvirt/images/'
 
 parser = argparse.ArgumentParser(description="Create QCOW2 images and spin " +
                                              "up VMs with them")
-parser.add_argument('image', metavar='image', type=str,
+parser.add_argument('image', type=str,
                     help='the name of an image in %s' % IMAGES_DIR)
+parser.add_argument('user_data', type=str,
+                    help='path to user-data file for cloud-init')
 parser.add_argument('-c', '--clean', action='store_true',
                     help='only clean up existing instances')
-parser.add_argument('-i','--images-dir', action='store',
+parser.add_argument('-i', '--images-dir', action='store',
                     help='[NOT IMPLEMENTED] alternate location for images')
 parser.add_argument('-m', '--meta-data', type=str, action='store',
-                    help='[NOT IMPLEMENTED] path to meta-data file for cloud-init')
+                    help='[NOT IMPLEMENTED] path to meta-data file for ' +
+                         'cloud-init')
 parser.add_argument('-n', '--num-instances', type=int, action='store',
                     help='number of image instances and VMs to create')
-parser.add_argument('-u', '--user-data', type=str, action='store',
-                    help='[NOT IMPLEMENTED] path to user-data file for cloud-init')
 args = parser.parse_args()
 
 
@@ -54,29 +90,36 @@ print "Found image at: %s" % fp_to_image
 for n in range(num_instances):
     instance_name = image_name + '-instance-' + str(n)
     instance_file = instance_name + image_type
+
     fp_to_instance = IMAGES_DIR + instance_file
     if instance_file in file_list:
-        print "Found existing image instance %s; it will now be removed" % instance_file
+        print "Removing existing instance %s; " % instance_file
         os.remove(fp_to_instance)
 
     print "Creating new image instance: %s" % fp_to_instance
-    subprocess.check_call(['qemu-img',
-                           'create',
-                           '-f', 'qcow2',
-                           '-o', 'backing_file=%s' % fp_to_image,
-                           fp_to_instance])
+    qemu_cmd = ['/usr/bin/qemu-img', 'create', '-f', 'qcow2', '-o',
+                'backing_file=%s' % fp_to_image, fp_to_instance]
+    qemu_p = subprocess.Popen(qemu_cmd, stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE)
+    out, err = qemu_p.communicate()
+    if qemu_p.returncode != 0:
+        print err
 
-    # print "Starting VM using new instance"
-    # subprocess.check_call(['virt-install',
-    #                        '--import',
-    #                        '--name', instance_name,
-    #                        '--ram', '2048',
-    #                        '--disk', 'path=%s,format=qcow2,bus=virtio' % fp_to_instance,
-    #                        '--disk', 'path=%s,device=cdrom' % fp_to_ci_iso,
-    #                        '--network', 'bridge=virbr0',
-    #                        '--os-type', 'linux',
-    #                        '--os-variant', 'rhel7'])
+    print "Creating new cloud-init ISO"
+    fp_to_ci_iso = generate_ci_iso(name=instance_name,
+                                   user_data=args.user_data)
+    if fp_to_ci_iso == 1:
+        sys.exit("ERROR: unable to generate cidata ISO")
 
-# virt-install --import --name rhel-atomic-cloud-7.1-3-instance-0 --ram 2048 --disk path=/var/lib/libvirt/images/rhel-atomic-cloud-7.1-3.x86_64-instance-0.qcow2,format=qcow2,bus=virtio --disk path=/var/lib/libvirt/images/rhel-atomic-cloud-7.1-3-0-cidata.iso,device=cdrom --network bridge=virbr0 --graphics vnc --os-type linux --os-variant rhel7
-
-# genisoimage -output /var/lib/libvirt/images/rhel-atomic-cloud-7.1-3-0-cidata.iso -volid cidata -joliet -rock user-data meta-data
+    print "Starting VM using new instance"
+    virt_cmd = ['/usr/bin/virt-install', '--import', '--name', instance_name,
+                '--ram', '2048', '--disk',
+                'path=%s,format=qcow2,bus=virtio' % fp_to_instance,
+                '--disk', 'path=%s,device=cdrom' % fp_to_ci_iso,
+                '--network', 'bridge=virbr0', '--os-type', 'linux',
+                '--os-variant', 'rhel7', '--noautoconsole']
+    virt_p = subprocess.Popen(virt_cmd, stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE)
+    out, err, = virt_p.communicate()
+    if virt_p.returncode != 0:
+        print err
